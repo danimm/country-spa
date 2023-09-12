c2ce3035ba8a6a5b0f11f3f8c086de7000ed666b
Daniel Munoz <danimm.dev@gmail.com>
2023-09-12 16:56:55 +0200

    service created

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { CountriesService } from "../../services/countries.service";
+import { Country } from "../../interfaces/country";
 
 @Component({
   selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = []
+
+  constructor(private countriesService: CountriesService) {}
+
 
   searchByCapital(term: string): void {
-    console.log('by capital page..')
-    console.log({ term })
+    this.countriesService
+      .searchCapital(term)
+      .subscribe(countries => {
+        this.countries = countries
+      })
   }
 }
